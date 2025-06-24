8213ea5cb45c678ad57b2fd78a10b42c402913a5
Tushar Tyagi <tushar.tyagi@example.com>
2025-06-25 02:02:58 +0530

    maths code add

--- new.js
+++ new.js
@@ -2,3 +2,14 @@ let x = 55;
 let y = 10;
 
 console.log(x + y); // Output: 65   
+
+
+console.log(x - y); // Output: 45
+console.log(x * y); // Output: 550
+console.log(x / y); // Output: 5.5
+console.log(x % y); // Output: 5
+console.log(x ** y); // Output: 3.1622776601683795e+55
+console.log(x++); // Output: 55
+console.log(x); // Output: 56
+console.log(++x); // Output: 57
+console.log(x--); // Output: 57
