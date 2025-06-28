38a658b2593418c1e69cefe90f8a499bc6127edc
realtushartyagi <tushartyagi7575@gmail.com>
2025-06-29 02:03:06 +0530

    code updated

--- New folder/y.js
+++ New folder/y.js
@@ -9,6 +9,15 @@ buttons.forEach(button => {
     if(e.target.id === 'grey'){
         body.style.backgroundColor = e.target.id;
     }
+     if(e.target.id === 'red'){
+        body.style.backgroundColor = e.target.id;
+    }
+     if(e.target.id === 'blue'){
+        body.style.backgroundColor = e.target.id;
+    }
+     if(e.target.id === 'green'){
+        body.style.backgroundColor = e.target.id;
+    }
   });
 });
 
