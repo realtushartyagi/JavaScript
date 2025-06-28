3729aba7984cb9c2787bfc22daef814d3f358eda
realtushartyagi <tushartyagi7575@gamil.com>
2025-06-29 01:34:55 +0530

    codes deleted

--- New folder/y.js
+++ New folder/y.js
@@ -1,15 +1,4 @@
-// let val = 10;
-// let val2 = 20;
-// let val3 = val1 + val2;
-// console.log(val3); // Output: 30
-// let val4 = val1 - val2;
-// console.log(val4); // Output: -10
-// let val5 = val1 * val2;
-// console.log(val5); // Output: 200
-// let val6 = val1 / val2;
-// console.log(val6); // Output: 0.5
-// let val7 = val1 % val2;
-// console.log(val7); // Output: 10
+
 
 
 const buttons = document.querySelectorAll('.button');
