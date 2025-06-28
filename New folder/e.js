8c0d77643a5dc598fbae35854b1e059ebba9c5a9
realtushartyagi <tushartyagi7575@gmail.com>
2025-06-29 02:12:34 +0530

    new codes in the new file

---
+++ New folder/e.js
@@ -0,0 +1,20 @@
+
+buttons.forEach(button => {
+    console.log(button);
+  button.addEventListener('click', (e) => {
+    console.log(e);
+    console.log(e.target);
+    if(e.target.id === 'grey'){
+        body.style.backgroundColor = e.target.id;
+    }
+     if(e.target.id === 'red'){
+        body.style.backgroundColor = e.target.id;
+    }
+     if(e.target.id === 'blue'){
+        body.style.backgroundColor = e.target.id;
+    }
+     if(e.target.id === 'green'){
+        body.style.backgroundColor = e.target.id;
+    }
+  });
+});
