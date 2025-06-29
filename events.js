bf33c0d15720aec35265dd2971bb7b5f1bcf0871
realtushartyagi <tushartyagi7575@gmail.com>
2025-06-30 01:56:11 +0530

    make a comment

--- events.js
+++ events.js
@@ -1,6 +1,7 @@
 document.querySelector('.images').addEventListener('click', function(event) {
     console.log(event.target.parentNode);
-    let removeit = event.target.parentNode;
-    removeit.remove();
-    console.log(removeit);
+    let removeIt = event.target.parentNode;
+    // removeIt.remove();
+    // console.log(removeIt);
+    removeIt.parentNode.removeChild(removeIt);
 })
