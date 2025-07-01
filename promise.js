0259561b0274c36075c2ccfef6605557e36b2274
realtushartyagi <tushartyagi7575@gmail.com>
2025-07-02 01:53:31 +0530

    code

--- promise.js
+++ promise.js
@@ -1,6 +1,10 @@
 const promiseOne = new Promise((resolve, reject) => {
     setTimeout(() => {
         console.log("Promise resolved after 2 seconds");
+        resolve("Promise is resolved");
 },1000)
 })
 
+promiseOne.then(() => {
+    console.log("This is the then block");
+})
